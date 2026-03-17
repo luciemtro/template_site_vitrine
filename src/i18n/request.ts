7b8e5e4e995d711bdfb2f878b2688673426a80f2
Nathan Lombardelli <nathanlombardelli@hotmail.com>
2026-03-17 23:37:26 +0100

    fix: locales

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,18 +1,14 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-
-const locales = ["fr", "en"];
+import { type Locale, locales } from "@/i18n/routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  // 1. On récupère la langue demandée
-  const locale = await requestLocale;
+  const locale = (await requestLocale) as Locale;
 
-  // 2. Si elle est absente ou non supportée, on déclenche une page 404
   if (!locale || !locales.includes(locale)) {
     notFound();
   }
 
-  // 3. À ce stade, TypeScript sait que `locale` est 100% une string valide
   return {
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default,
