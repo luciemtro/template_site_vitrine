32829d7bc01130be2b880356a71d4ff59df874dc
Nathan Lombardelli <nathanlombardelli@hotmail.com>
2026-03-17 23:33:29 +0100

    feat: fontawsome

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,5 @@
+import { faArrowRightLong, faGift } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "next-intl";
 
 export default function HomePage() {
@@ -5,8 +7,10 @@ export default function HomePage() {
 
   return (
     <main>
+      <FontAwesomeIcon icon={faGift} className="w-6" />
       <h1>{t("title")}</h1>
       <p>{t("description")}</p>
+      <FontAwesomeIcon icon={faArrowRightLong} className="w-6" />
     </main>
   );
 }
